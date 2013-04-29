10ca5abac7a9fed8a3dd6aebd432311d22472d6e
Arming Huang <nikom@socket9.com>
2013-04-30 04:28:23 +0700

    First add project

---
+++ Site/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.socket9.com:8081/admin/');
+define('HTTP_CATALOG', 'http://www.socket9.com:8081/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.socket9.com:8081/admin/');
+define('HTTPS_CATALOG', 'http://www.socket9.com:8081/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\inetpub\wwwroot\9Naliga/admin/');
+define('DIR_SYSTEM', 'C:\inetpub\wwwroot\9Naliga/system/');
+define('DIR_DATABASE', 'C:\inetpub\wwwroot\9Naliga/system/database/');
+define('DIR_LANGUAGE', 'C:\inetpub\wwwroot\9Naliga/admin/language/');
+define('DIR_TEMPLATE', 'C:\inetpub\wwwroot\9Naliga/admin/view/template/');
+define('DIR_CONFIG', 'C:\inetpub\wwwroot\9Naliga/system/config/');
+define('DIR_IMAGE', 'C:\inetpub\wwwroot\9Naliga/image/');
+define('DIR_CACHE', 'C:\inetpub\wwwroot\9Naliga/system/cache/');
+define('DIR_DOWNLOAD', 'C:\inetpub\wwwroot\9Naliga/download/');
+define('DIR_LOGS', 'C:\inetpub\wwwroot\9Naliga/system/logs/');
+define('DIR_CATALOG', 'C:\inetpub\wwwroot\9Naliga/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'socket9#');
+define('DB_DATABASE', '9naliga');
+define('DB_PREFIX', '');
+?>
---
+++ Site/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.socket9.com:8081/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.socket9.com:8081/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\inetpub\wwwroot\9Naliga/catalog/');
+define('DIR_SYSTEM', 'C:\inetpub\wwwroot\9Naliga/system/');
+define('DIR_DATABASE', 'C:\inetpub\wwwroot\9Naliga/system/database/');
+define('DIR_LANGUAGE', 'C:\inetpub\wwwroot\9Naliga/catalog/language/');
+define('DIR_TEMPLATE', 'C:\inetpub\wwwroot\9Naliga/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\inetpub\wwwroot\9Naliga/system/config/');
+define('DIR_IMAGE', 'C:\inetpub\wwwroot\9Naliga/image/');
+define('DIR_CACHE', 'C:\inetpub\wwwroot\9Naliga/system/cache/');
+define('DIR_DOWNLOAD', 'C:\inetpub\wwwroot\9Naliga/download/');
+define('DIR_LOGS', 'C:\inetpub\wwwroot\9Naliga/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'socket9#');
+define('DB_DATABASE', '9naliga');
+define('DB_PREFIX', '');
+?>
