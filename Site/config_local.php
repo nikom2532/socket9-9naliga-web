fb8d634107c2978f891fd02b0df050e52b2ae88b
nikom2532 <nikom2532@hotmail.com>
2013-05-25 01:35:58 +0700

    [-- above --]

---
+++ Site/config_local.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/9naliga/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/9naliga/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\9naliga/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\9naliga/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\9naliga/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\9naliga/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\9naliga/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\9naliga/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\9naliga/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\9naliga/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\9naliga/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\9naliga/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '0220');
+define('DB_DATABASE', '9naliga');
+define('DB_PREFIX', '');
+?>
